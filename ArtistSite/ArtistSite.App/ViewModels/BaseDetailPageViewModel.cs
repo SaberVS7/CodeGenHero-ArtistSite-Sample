dc6fcad7e45e438caa8133fc0ecc33c3996ee484
Robert Schroeder <RobertS@msctek.com>
2022-06-27 17:53:20 -0500

    Add project files.

---
+++ ArtistSite/ArtistSite.App/ViewModels/BaseDetailPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using ArtistSite.Shared.Constants;
+using ArtistSite.Shared.DTO;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtistSite.App.ViewModels
+{
+    public class BaseDetailPageViewModel : BaseViewModel
+    {
+        [Parameter]
+        public int ArtistId { get; set; } = 0;
+
+        public Artist Artist { get; set; }
+
+        public IList<Artwork> Artworks { get; set; }
+
+        [Parameter]
+        public int MediumId { get; set; } = 0;
+
+        public Medium Medium { get; set; }
+
+        protected async Task PopulateArtistAsync(bool getArtworks = false)
+        {
+            var getRelatedEntities = getArtworks ? Enums.RelatedEntitiesType.ImmediateChildren : Enums.RelatedEntitiesType.None;
+
+            var result = await WebApiDataServiceAS.GetArtistAsync(ArtistId, getRelatedEntities);
+
+            if (result.IsSuccessStatusCode)
+            {
+                Artist = result.Data;
+            }
+        }
+
+        protected async Task PopulateMediumAsync()
+        {
+            var result = await WebApiDataServiceAS.GetMediumAsync(MediumId);
+
+            if (result.IsSuccessStatusCode)
+            {
+                Medium = result.Data;
+            }
+        }
+    }
+}
---
+++ ArtistSite/ArtistSite.Repository/Repositories/ASRepositoryCustom.cs
@@ -0,0 +1,44 @@
+﻿using ArtistSite.Repository.Entities;
+using ArtistSite.Shared.Constants;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtistSite.Repository.Repositories
+{
+    public partial class ASRepository
+    {
+        partial void ApplyRelatedEntitiesType(
+            ref IQueryable<Artist> qry, Enums.RelatedEntitiesType relatedEntitiesType)
+        {
+            switch (relatedEntitiesType)
+            {
+                case Enums.RelatedEntitiesType.ImmediateChildren:
+                    qry = qry.Include(x => x.Artworks).AsQueryable();
+                    break;
+
+                default:
+
+                    break;
+            }
+        }
+
+        partial void ApplyRelatedEntitiesType(
+            ref IQueryable<Artwork> qry, Enums.RelatedEntitiesType relatedEntitiesType)
+        {
+            switch (relatedEntitiesType)
+            {
+                case Enums.RelatedEntitiesType.ImmediateChildren:
+                    qry = qry.Include(x => x.Artist).AsQueryable();
+                    break;
+
+                default:
+
+                    break;
+            }
+        }
+    }
+}
